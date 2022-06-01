963c3556eb9d4812cb4ce047b29af3ad9794b6ba
Chezz-B <brujic.caslav@gmail.com>
2022-06-02 00:21:26 +0200

    PlusMinus function, cleaned-up code for numbers click event

--- script.js
+++ script.js
@@ -2,15 +2,7 @@
 
 const display = document.getElementById("display");
 let sum = 0;
-console.log(typeof sum);
 let current = 0;
-
-
-const clearBtn = document.getElementById("clearBtn");
-clearBtn.addEventListener("click", function(){
-  display.textContent = 0;
-  sum = 0;
-});
 
 
 
@@ -18,35 +10,27 @@ const numbers = Array.from(document.querySelectorAll(".number"));
 
 numbers.forEach(function(number){
   number.addEventListener("click", function(){
-    if(sum == 0){
-      current = Number(number.value);
-      // console.log(current);
-      sum = String(current);
-      // console.log(sum);
-      display.textContent = sum;
-    } else {
-      current = Number(number.value);
-      // console.log(current);
-      sum += current;
-      // console.log(sum);
-      display.textContent = sum;
-    }
-  })
+    sum == 0 ? sum = number.value : sum += number.value;
+    display.textContent = sum;
+  });
+});
+
+
+
+const clearBtn = document.getElementById("clearBtn");
+
+clearBtn.addEventListener("click", function(){
+  display.textContent = 0;
+  sum = 0;
+});
+
+
+
+const plusMinus = document.getElementById("plusMinus");
+
+plusMinus.addEventListener("click", function(){
+  if(sum != 0){
+    sum > 0 ? sum = `-${sum}` : sum = sum.slice(1);
+    display.textContent = sum;
+  }
 })
-
-
-
-// numbers.forEach(function(number){
-//   let numValue = number.value;
-//   console.log(numValue);
-//   number.addEventListener("click", function(){
-//     if(sum = 0){
-//     display.textContent = numValue;
-//     sum += numValue;
-//     } else {
-//       let newSum = String(sum);
-//       newSum += numValue;
-//       display.textContent = newSum;
-//     }
-//   })
-// })
